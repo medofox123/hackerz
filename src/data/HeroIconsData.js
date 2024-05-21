1447a86f4858fe77a52f5e3acc5f821a19d7ec17
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-05-22 01:48:53 +0300

    Add files via upload

---
+++ src/data/HeroIconsData.js
@@ -0,0 +1,35 @@
+//Only designed to support 5 icons.
+//If you want to add more. youll need to mify styles
+//Every class (example: icon-4), must be styled to preserve responsive
+export const HeroIconsSocial = [
+    {
+        name:"github",
+        class: "icon-1",
+        url: "https://github.com/medofox123",
+        src: "./svg/github-light.svg"
+    },
+    {
+        name:"insta",
+        class: "icon-2",
+        url: "https://www.instagram.com/x_mr.fox_x?r=nametag",
+        src: "./svg/instafox.svg"
+    },
+    {
+        name:"linkedin",
+        class: "icon-3",
+        url: "https://www.linkedin.com/in/medo-hackerz-1295592b2",
+        src: "./svg/linkedin.svg"
+    },
+    {
+        name:"WhatsApp",
+        class: "icon-4",
+        url: "https://wa.me/+201028186564?text=hello+fox+your+web+is+verry+cool",
+        src: "./svg/whatsappColor.svg"
+    },
+    {
+        name:"youtube",
+        class: "icon-5",
+        url: "https://www.youtube.com/@X_MR_FOX_X",
+        src: "./svg/youtube-solid.svg"
+    },
+]
