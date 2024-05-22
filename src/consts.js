687d8294a8bd67c9134ddaa0e4e0362a512d8111
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-05-22 11:45:00 +0300

    Add files via upload

---
+++ src/consts.js
@@ -0,0 +1,5 @@
++// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'DEVFOX';
+export const SITE_DESCRIPTION = 'This website was programmed and designed by the programmer Abdelhamed.';
